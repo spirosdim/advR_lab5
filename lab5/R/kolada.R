28d85c46ee70ff87df38011d750d815674ceeb93
lmxblur <lmxgt5@gmail.com>
2019-10-02 12:42:08 +0200

    Adding get_id method

--- lab5/R/kolada.R
+++ lab5/R/kolada.R
@@ -1,11 +1,17 @@
 kolada <- setRefClass("kolada",
-                      filed = list(municipal="character"),
+                      field = list(municipal="character"),
                       methods = list(
                         initialize = function(municipal){
                           municipal <<- municipal
                         },
                         get_id = function(){
-                          
+                          path_mu = "http://api.kolada.se/v2/municipality?"
+                          api_raw_ret <- httr::GET(url = path_mu,
+                                                   query = list(title=municipal))
+                          api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                          id_json <- ef <- jsonlite::fromJSON(api_text,flatten = TRUE)
+                          id <- id_json$values$id
+                          return(id)
                         }
-                        )
-                      ))
+                      )
+)
