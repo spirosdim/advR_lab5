e0bd6fbc62b739161574c6408c7758bed27017aa
lmxblur <lmxgt5@gmail.com>
2019-10-02 13:02:43 +0200

    Adding get_school method

--- lab5/R/kolada.R
+++ lab5/R/kolada.R
@@ -9,9 +9,20 @@ kolada <- setRefClass("kolada",
                           api_raw_ret <- httr::GET(url = path_mu,
                                                    query = list(title=municipal))
                           api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
-                          id_json <- ef <- jsonlite::fromJSON(api_text,flatten = TRUE)
+                          id_json <- jsonlite::fromJSON(api_text,flatten = TRUE)
                           id <- id_json$values$id
                           return(id)
+                        },
+                        get_school = function(){
+                          path_sk <- "http://api.kolada.se/v2/ou?"
+                          id <- get_id()
+                          api_raw_ret <- httr::GET(url = path_sk,
+                                                   query = list(municipality=id,title="skola"))
+                          api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                          skola_json <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                          sk_list <- skola_json['values.title']
+                          colnames(sk_list) <- c('school')
+                          return(sk_list)
                         }
                       )
 )
