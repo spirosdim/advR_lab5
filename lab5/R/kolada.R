453b10fd1f86a2a4040c98a7145a18035f3fd94a
lmxblur <lmxgt5@gmail.com>
2019-10-03 15:24:48 +0200

    Adding new mathod and small changes in class

--- lab5/R/kolada.R
+++ lab5/R/kolada.R
@@ -2,17 +2,13 @@
 #' 
 #' @import httr
 #' @import jsonlite
-#' @field municipal character: A given municipality
 #' @export kolada
 #' @exportClass kolada
 kolada <- setRefClass("kolada",
-                      field = list(municipal="character"),
+                      field = list(),
                       methods = list(
-                        initialize = function(municipal){
-                          municipal <<- municipal
-                        },
-                        get_id = function(){
-                          'Initialize the values'
+                        get_id = function(municipal){
+                          'Get the id of a municipality'
                           path_mu = "http://api.kolada.se/v2/municipality?"
                           api_raw_ret <- httr::GET(url = path_mu,
                                                    query = list(title=municipal))
@@ -21,10 +17,10 @@ kolada <- setRefClass("kolada",
                           id <- id_json$values$id
                           return(id)
                         },
-                        get_school = function(){
-                          'Get the school list with given municipality'
+                        get_skola = function(municipal){
+                          'Get the school list in a municipality'
                           path_sk <- "http://api.kolada.se/v2/ou?"
-                          id <- get_id()
+                          id <- get_id(municipal)
                           api_raw_ret <- httr::GET(url = path_sk,
                                                    query = list(municipality=id,title="skola"))
                           api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
@@ -32,6 +28,16 @@ kolada <- setRefClass("kolada",
                           sk_list <- skola_json['values.title']
                           colnames(sk_list) <- c('school')
                           return(sk_list)
+                        },
+                        get_municipality_list = function(){
+                          'Get the complete municipality list'
+                          path_get_mu <- "http://api.kolada.se/v2/municipality"
+                          api_raw_ret <- httr::GET(url = path_get_mu)
+                          api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                          municipality_list_raw <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                          municipality_list <- municipality_list_raw[,c(2,3)]
+                          colnames(municipality_list) = c('id','municipality')
+                          return(municipality_list)
                         }
                       )
 )
