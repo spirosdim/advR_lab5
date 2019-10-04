7694f89af675d1745aa941af246f786181ef5cd9
spirosdim <spirosdmtr@gmail.com>
2019-10-04 14:50:32 +0200

    add get_numb method

--- lab5/R/kolada.R
+++ lab5/R/kolada.R
@@ -34,6 +34,76 @@ kolada <- setRefClass("kolada",
                           sk_list <- skola_json['values.title']
                           colnames(sk_list) <- c('school')
                           return(sk_list)
+                        },
+                        get_numb = function(Mname, op){
+                          'Get the complete municipality list'
+                          #1 dead
+                          #2 moving net
+                          #3 born / 1000
+                          #4 born numb
+                          try(if (!(op %in% c(1:4))) stop("not one of the provided options"))
+                          
+                          start_path <- "http://api.kolada.se/v2/data/kpi"
+                          if (op==1){
+                            n<-"N01805"
+                            uurl <- paste(start_path, "/", n, "/municipality/", get_id(Mname), sep = "")
+                            api_raw_ret <- httr::GET(url = uurl)
+                            api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                            polulation_list_raw <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                            
+                            vvec <- rep(0, length(polulation_list_raw$values.values))
+                            for (i in 1:length(polulation_list_raw$values.values)){
+                              vvec[i] <- polulation_list_raw$values.values[[i]][[4]]
+                            }
+                            
+                            df <- data.frame("period"=polulation_list_raw$values.period)
+                            df$values <- vvec 
+                            
+                          }else if (op==2){
+                            n<-"N01800"
+                            uurl <- paste(start_path, "/", n, "/municipality/", get_id(Mname), sep = "")
+                            api_raw_ret <- httr::GET(url = uurl)
+                            api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                            polulation_list_raw <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                            
+                            vvec <- rep(0, length(polulation_list_raw$values.values))
+                            for (i in 1:length(polulation_list_raw$values.values)){
+                              vvec[i] <- polulation_list_raw$values.values[[i]][[4]][[3]]
+                            }
+                            df <- data.frame("period"=polulation_list_raw$values.period)
+                            df$values <- vvec 
+                            
+                            
+                          }else if (op==3){
+                            n<-"N02951"
+                            uurl <- paste(start_path, "/", n, "/municipality/", get_id(Mname), sep = "")
+                            api_raw_ret <- httr::GET(url = uurl)
+                            api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                            polulation_list_raw <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                            
+                            vvec <- rep(0, length(polulation_list_raw$values.values))
+                            for (i in 1:length(polulation_list_raw$values.values)){
+                              vvec[i] <- polulation_list_raw$values.values[[i]][[4]]
+                            }
+                            df <- data.frame("period"=polulation_list_raw$values.period)
+                            df$values <- vvec 
+                            
+                          }else {
+                            n<-"N01804"
+                            uurl <- paste(start_path, "/", n, "/municipality/", get_id(Mname), sep = "")
+                            api_raw_ret <- httr::GET(url = uurl)
+                            api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
+                            polulation_list_raw <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
+                            
+                            vvec <- rep(0, length(polulation_list_raw$values.values))
+                            for (i in 1:length(polulation_list_raw$values.values)){
+                              vvec[i] <- polulation_list_raw$values.values[[i]][[4]]
+                            }
+                            df <- data.frame("period"=polulation_list_raw$values.period)
+                            df$values <- vvec 
+                          }
+                          
+                          return(df)
                         }
                         
                         
