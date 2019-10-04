f7c98c3544626f1ee39f03e5fbead90c61fb9915
spirosdim <spirosdmtr@gmail.com>
2019-10-05 01:03:21 +0200

    change tests

--- lab5/tests/testthat/testkolada.R
+++ lab5/tests/testthat/testkolada.R
@@ -8,18 +8,18 @@ Kobj <- kolada$new()
 
 ## User input
 test_that("Wrong user input is detected from get_id()", {
-  expect_error(K$get_id("New york"))
-  expect_error(K$get_id("London"))
+  expect_error(Kobj$get_id("New york"))
+  expect_error(Kobj$get_id("London"))
 })
 
 
 test_that("Wrong user input is detected from get_numb()", {
-  expect_error(K$get_numb("New york",1))
-  expect_error(K$get_numb("Ale", 5))
+  expect_error(Kobj$get_numb("New york",1))
+  expect_error(Kobj$get_numb("Ale", 5))
 })
 
 ## Testing that the function allways return a given value for a specific set of inputs.
 test_that("Wrong user input is detected from get_id()", {
-  expect_error(K$get_id("Linkoping")=="0580")
-  expect_error(K$get_id("Ale")=="1441")
+  expect_error(Kobj$get_id("Linkoping")=="0580")
+  expect_error(Kobj$get_id("Ale")=="1441")
 })
