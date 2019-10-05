122fc714270cf7ee62ffc1b13daa64672f705561
spirosdim <spirosdmtr@gmail.com>
2019-10-05 19:43:30 +0200

    change test messages

--- lab5/tests/testthat/testkolada.R
+++ lab5/tests/testthat/testkolada.R
@@ -8,7 +8,6 @@ test_that("Wrong user input is detected from get_id()", {
   expect_error(Kobj$get_id("London"))
 })
 
-
 test_that("Wrong user input is detected from get_stats()", {
   Kobj <- kolada$new()
   expect_error(Kobj$get_stats("New york",1))
@@ -16,37 +15,37 @@ test_that("Wrong user input is detected from get_stats()", {
 })
 
 ## User output
-test_that("Wrong user output is detected from get_id()", {
+test_that("Wrong return output type is detected from get_id()", {
   Kobj <- kolada$new()
   expect_true(is.character(Kobj$get_id("Ale")))
   expect_true(is.character(Kobj$get_id("Linköping")))
 })
 
-test_that("Wrong user output is detected from get_skola()", {
+test_that("Wrong return output type is detected from get_skola()", {
   Kobj <- kolada$new()
   expect_true(is.data.frame(Kobj$get_skola("Ale")))
   expect_true(is.data.frame(Kobj$get_skola("Linköping")))
 })
 
-test_that("Wrong user output is detected from get_municipality_list()", {
+test_that("Wrong return output type is detected from get_municipality_list()", {
   Kobj <- kolada$new()
   expect_true(is.data.frame(Kobj$get_municipality_list()))
 })
 
-test_that("Wrong user output is detected from get_stats()", {
+test_that("Wrong return output type is detected from get_stats()", {
   Kobj <- kolada$new()
   expect_true(is.data.frame(Kobj$get_stats("Ale", 2)))
   expect_true(is.data.frame(Kobj$get_stats("Linköping", 4)))
 })
 
 ## Testing that the function allways return a given value for a specific set of inputs.
-test_that("Wrong user input is detected from get_id()", {
+test_that("Wrong return value for a specific input from get_id()", {
   Kobj <- kolada$new()
   expect_true(Kobj$get_id("Linköping")=="0580")
   expect_true(Kobj$get_id("Ale")=="1440")
 })
 
-test_that("Wrong user input is detected from get_skola()", {
+test_that("Wrong return value for a specific input from get_skola()", {
   Kobj <- kolada$new()
   expect_true(Kobj$get_skola("Bjurholm")==data.frame("school"=c("Castorskolan")))
   expect_true(all(Kobj$get_skola("Sorsele")==data.frame("school"=c("Vindelälvsskolan F-6","Blattnicksele skola",
